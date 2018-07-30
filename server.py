853bae93a2be127eddca789a31509dd0c53a73a1
Nick Burkhart <nickburkhart@gmail.com>
2018-07-29 22:37:30 -0400

    server react app build files

--- server.py
+++ server.py
@@ -1,9 +1,19 @@
-from flask import Flask, request
+from flask import Flask, request, send_from_directory
 from flask_socketio import SocketIO, emit
+import os
     
-app = Flask(__name__)
+app = Flask(__name__, static_folder='chat-app/build')
 socketio = SocketIO(app)
 
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def serve(path):
+		print('path: ', path)
+		if path != "" and os.path.exists("chat-app/build/" + path):
+				return send_from_directory('chat-app/build', path)
+		else:
+				return send_from_directory('chat-app/build', 'index.html')
+				
 @socketio.on('disconnect')
 def disconnect():
     emit('left', { 'sid': request.sid }, broadcast=True)
